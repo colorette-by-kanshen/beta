884012a0c6c4bc01550fa21301d1943e40c2f98a
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-02-25 19:06:31 +0100

    Fixed some issues with changeTheme.js
    - Fixed invalid selectors (Issue #3)

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -1,7 +1,7 @@
 function changeTheme() {
-    var input = document.getElementById("color-input").value;
-    var formField = document.getElementById("color-input");
-    var button = document.querySelector("button");
+    var input = document.getElementById("hex-input").value;
+    var formField = document.getElementById("hex-input");
+    var button = document.querySelector("form");
     var logo = document.getElementById("logo")
   
     formField.style.backgroundColor = "#" + input;
