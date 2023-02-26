bcd00038f61ee35fae0eb345fe429c02cd6bc1b0
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-02-26 13:49:38 +0100

    Added Input Color Check

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -7,4 +7,24 @@ function changeTheme() {
     formField.style.backgroundColor = "#" + input;
     button.style.backgroundColor = "#" + input;
     logo.style.backgroundColor = "#" + input;
+    updateLogoColor;
+
+}
+
+function updateLogoColor() {
+  var logo = document.getElementById("logo");
+  var header = document.querySelector("header");
+  var computedStyle = getComputedStyle(logo);
+  var backgroundColor = computedStyle.backgroundColor;
+  var colorValues = backgroundColor.match(/\d+/g).map(Number);
+  var isLight = colorValues.every(function(value) {
+    return value >= 150;
+  });
+  if (isLight) {
+    logo.src = "img/logo-black.png";
+    header.style.backgroundColor = "#000000";
+  } else {
+    logo.src = "img/logo-white.png";
+    header.style.backgroundColor = "#FFFFFF";
+  }
 }
