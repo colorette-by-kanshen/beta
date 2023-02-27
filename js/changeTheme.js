b39f667555a5870c9396216db203f48a3981bc45
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-02-27 19:24:10 +0100

    Updated changeTheme.js
    - Fixed updateLogoColor not working (Issue #10)

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -12,33 +12,35 @@ function changeTheme() {
 
 }
 
+function updateLogoColor() {
   var urlParams = new URLSearchParams(window.location.search);
   var color = urlParams.get("color");
   if (color) {
-  var formField = document.getElementById("hex-input");
-  var button = document.querySelector("button");
-  var logo = document.getElementById("logo");
-  var header = document.querySelector("header");
+    var formField = document.getElementById("hex-input");
+    var button = document.querySelector("button");
+    var logo = document.getElementById("logo");
+    var header = document.querySelector("header");
 
-  formField.value = color;
-  formField.style.background = "#" + color;
-  button.style.background = "#" + color;
-  logo.style.background = "#" + color;
+    formField.value = color;
+    formField.style.background = "#" + color;
+    button.style.background = "#" + color;
+    logo.style.background = "#" + color;
 
-  var r = parseInt(color.substring(0,2), 16);
-  var g = parseInt(color.substring(2,4), 16);
-  var b = parseInt(color.substring(4,6), 16);
+    var r = parseInt(color.substring(0, 2), 16);
+    var g = parseInt(color.substring(2, 4), 16);
+    var b = parseInt(color.substring(4, 6), 16);
 
-  // test if all color channels are at least 150
-  if (redValue >= 150 && greenValue >= 150 && blueValue >= 150) {
-    // set logo to black version and header background to black
-    logo.src = "img/logo-black.png";
-    header.style.backgroundColor = "#000000";
-    formField.style.color = "#000000"
-  } else {
-    // set logo to white version and header background to white
-    logo.src = "img/logo-white.png";
-    header.style.backgroundColor = "#FFFFFF";
-    formField.style.color = "#FFFFFF"
+    // test if all color channels are at least 150
+    if (redValue >= 150 && greenValue >= 150 && blueValue >= 150) {
+      // set logo to black version and header background to black
+      logo.src = "img/logo-black.png";
+      header.style.backgroundColor = "#000000";
+      formField.style.color = "#000000"
+    } else {
+      // set logo to white version and header background to white
+      logo.src = "img/logo-white.png";
+      header.style.backgroundColor = "#FFFFFF";
+      formField.style.color = "#FFFFFF"
+    }
   }
 }
