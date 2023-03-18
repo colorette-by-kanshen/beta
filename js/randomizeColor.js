00f502280d72e29da5381cace134ce04c2549377
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-18 12:34:13 +0100

    Update randomizeColor.js

--- js/randomizeColor.js
+++ js/randomizeColor.js
@@ -1,6 +1,6 @@
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
-    let color;
+    let color = "";
     for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
