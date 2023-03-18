92f79f393e8cdb3eb6ce198b30cb7129fbc94ea0
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-18 12:52:21 +0100

    Update setColor.js

--- js/setColor.js
+++ js/setColor.js
@@ -2,5 +2,6 @@ function setColor() {
   var input = document.getElementById("hex-input").value;
   var url = new URL(window.location.href);
   url.searchParams.set("color", input);
+  url.searchParams.delete("random");
   window.location.href = url.href;
 }
