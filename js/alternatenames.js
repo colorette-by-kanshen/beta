ce66bd3e60f211de6484287e397c01d1984071ca
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-19 17:29:53 +0100

    Update alternatenames.js

--- js/alternatenames.js
+++ js/alternatenames.js
@@ -1,46 +1,14 @@
-async function checkAlternateNames() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const colorParam = urlParams.get('color');
-    
-    try {
-        const response = await fetch('json/colornames.json')
-        const colors = await response.json();
-
-        const colorsArray = Object.values(colors);
-
-        let matchingColors = colorsArray.filter(color => color.hex.toUpperCase() === colorParam.toUpperCase());
-
-        const alternateNamesDiv = document.getElementById('alternatenames');
-
-        if (matchingColors.length === 0) {
-            alternateNamesDiv.style.display = 'none';
-        } else if (matchingColors.length === 1) {
-            alternateNamesDiv.style.display = 'flex';
-            const placeholderOneSpan = document.getElementById('placeholderone');
-            placeholderOneSpan.textContent = 'Did you know that ';
-            const hexSpan = document.getElementById('hex');
-            hexSpan.style.color = colorParam;
-            hexSpan.textContent = colorParam;
-            const placeholderTwoSpan = document.getElementById('placeholdertwo');
-            placeholderTwoSpan.textContent = ' can also be interpreted as ';
-            const alternativeNamesSpan = document.getElementById('alternativenames');
-            alternativeNamesSpan.style.color = colorParam;
-            alternativeNamesSpan.textContent = matchingColors[0].name;
-        } else {
-            alternateNamesDiv.style.display = 'flex';
-            const placeholderOneSpan = document.getElementById('placeholderone');
-            placeholderOneSpan.textContent = 'Did you know that ';
-            const hexSpan = document.getElementById('hex');
-            hexSpan.style.color = colorParam;
-            hexSpan.textContent = colorParam;
-            const placeholderTwoSpan = document.getElementById('placeholdertwo');
-            placeholderTwoSpan.textContent = ' can also be interpreted as ';
-            const alternativeNamesSpan = document.getElementById('alternativenames');
-            alternativeNamesSpan.style.color = colorParam;
-            alternativeNamesSpan.textContent = matchingColors.map(color => color.codeName).join(' | ');
-        };
-    }
-    catch (error) {
-        console.log(error);
+async function loadColors() {
+    const response = await fetch('json/colornames.json');
+    const colorsJson = await response.json();
+    const colorsObject = {};
+    colorsJson.forEach(color => {
+        colorsObject[color.hex] = color;
+    });
+    const colors = await loadColors();
+    const color = urlParams.get('color');
+    const matchingColor = colors[colorHex];
+    if (matchingColor) {
+        console.log(matchingColor.name);
     }
 }
