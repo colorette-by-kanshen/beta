f317d311fc744b846e68d67ddf95df1646bfcbae
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-19 13:35:54 +0100

    Update alternatenames.js

--- js/alternatenames.js
+++ js/alternatenames.js
@@ -2,7 +2,9 @@ const urlParams = new URLSearchParams(window.location.search);
 const colorParam = urlParams.get('color');
 const colors = fetch('json/colornames.json').then(response => response.json());
 
-let matchingColors = colors.filter(color => color.hex() === colorParam.toUpperCase());
+const colorsArray = Object.values(colors);
+
+let matchingColors = colorsArray.filter(color => color.hex.toUpperCase() === colorParam.toUpperCase());
 
 const alternateNamesDiv = document.getElementById('alternatenames');
 
