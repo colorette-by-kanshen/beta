5811267a7e0ba3f7694c52af77c1826177b840d1
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-19 18:04:56 +0100

    Update alternatenames.js

--- js/alternatenames.js
+++ js/alternatenames.js
@@ -1,6 +1,6 @@
 function alternateNames(color) {
     // Load the json file
-    const colornames = require('json/colornames.json');
+    const colornames = fetch('json/colornames.json');
 
     // Compare the color parameter to all hex values in the objects of the json file
     for (let i = 0; i < colornames.length; i++) {
