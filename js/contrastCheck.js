1841fd9555a273197ad2c2b11c6c4b1a087de759
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-23 15:35:21 +0100

    Update contrastCheck.js

--- js/contrastCheck.js
+++ js/contrastCheck.js
@@ -12,22 +12,8 @@ function checkContrast(color) {
     const blueRGB = parseInt(blueHEX, 16);
     const greenRGB = parseInt(greenHEX, 16);
 
-    // Calculate the relative luminance of the input color
-    const luminance = (0.2126 * redRGB + 0.7152 * greenRGB + 0.0722 * blueRGB) / 255;
+    // Calculate the ContrastIndex
+    const contrastIndex = (redRGB + greenRGB + blueRGB) / 3 / 2.55 + (((redRGB + greenRGB + blueRGB) / 3 / 2.55) - 100).toFixed(1).Number()
 
-    // Calculate the contrast ratios against black and white
-    const whiteContrast = (luminance + 0.05) / 1.05;
-    const blackContrast = 1.05 / (luminance + 0.05);
-    const whiteContrastPercentage = whiteContrast * 100;
-    const blackContrastPercentage = blackContrast * 100;
-
-    const woc = document.querySelector(".contrastcheck.contrastwhite.contrasttop")
-    const boc = document.querySelector(".contrastcheck.contrastblack.contrasttop")
-    const cow = document.querySelector(".contrastcheck.contrastwhite.contrastbottom")
-    const cob = document.querySelector(".contrastcheck.contrastblack.contrastbottom")
-
-    woc.textContent = whiteContrastPercentage;
-    cow.textContent = "out of 100 points";
-    boc.textContent = blackContrastPercentage;
-    cob.textContent = "out of 100 points";
+    console.log(contrastIndex)
 }
