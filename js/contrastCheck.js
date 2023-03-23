1dfc969aac6687e92556e78551e6983d7a1d1104
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-23 17:56:57 +0100

    Update contrastCheck.js

--- js/contrastCheck.js
+++ js/contrastCheck.js
@@ -13,7 +13,11 @@ function checkContrast(color) {
     const greenRGB = parseInt(greenHEX, 16);
 
     // Calculate the ContrastIndex
-    const contrastIndex = ((redRGB + greenRGB + blueRGB) / 3 / 2.55 + (((redRGB + greenRGB + blueRGB) / 3 / 2.55) - 100)).toFixed(1)
+    const contrastIndex = ((redRGB + greenRGB + blueRGB) / 3 / 2.55 + (((redRGB + greenRGB + blueRGB) / 3 / 2.55) - 100))
 
-    console.log(contrastIndex)
+    // Set Elements Constants and Values
+    const whiteContrastBox = document.querySelector("#contrastWhite")
+    const blackContrastBox = document.querySelector("#contrastblack")
+    whiteContrastBox.textContent = contrastIndex.toFixed(1)
+    blackContrastBox.textContent = "ON A SCALE FROM -100 TO 100"
 }
