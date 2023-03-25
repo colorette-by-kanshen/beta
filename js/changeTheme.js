0e5f1ab6622b0f9b97dac98fa91aec17b0a4de8a
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-25 09:48:45 +0100

    Update changeTheme.js

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -37,7 +37,7 @@ function updateLogoColor(color) {
 }
 
 function updateToggleSwitch(color) {
-  var toggleSwitchAfter = document.querySelector("span.slider.round:checked+")
+  var toggleSwitchAfter = document.querySelector("input:active>span.slider.round")
   var toggleSwitchButton = document.querySelector("span#slidercircle")
 
   var r = parseInt(color.substring(0, 2), 16);
