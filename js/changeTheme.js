1ae659a6ac8cb743fa39e2b593277262d4a19eb6
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-25 09:14:48 +0100

    Update changeTheme.js

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -35,20 +35,20 @@ function updateLogoColor(color) {
   }
 }
 
-function updateAltCSSColor(color) {
-  const AlternateCSSDIV = getElementById("alternatenames")
-  const placeholderOneSpan = document.getElementById('placeholderone');
-  const placeholderTwoSpan = document.getElementById('placeholdertwo');
-  // test if all color channels are at least 150
+function updateToggleSwitch(color) {
+  var toggleSwitchAfter = document.querySelector("input:checked+.slider")
+  var toggleSwitchButton = document.querySelector(".slider:before")
+
+  var r = parseInt(color.substring(0, 2), 16);
+  var g = parseInt(color.substring(2, 4), 16);
+  var b = parseInt(color.substring(4, 6), 16);
+
   if (r >= 150 && g >= 150 && b >= 150) {
-    // set AlternateCSSDIV Color to Dark Mode
-    AlternateCSSDIV.backgroundColor = "#000000";
-    placeholderOneSpan.color = "#FFFFFF";
-    placeholderTwoSpan.color = "#FFFFFF"
+    // set logo to black version and header background to black
+    toggleSwitchButton.style.backgroundColor = "#000000"
   } else {
-    // set AlternateCSSDIV Color to Light Mode
-    AlternateCSSDIV.backgroundColor = "#FFFFFF";
-    placeholderOneSpan.color = "#000000";
-    placeholderTwoSpan.color = "#000000"
+    // set logo to white version and header background to white
+    toggleSwitchButton.style.backgroundColor = "#FFFFFF"
   }
+  toggleSwitchAfter.style.backgroundColor = color
 }
