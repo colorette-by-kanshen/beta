bcb21cf51d2d06e9c6a73174144259859cb99faf
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-25 11:41:05 +0100

    Fixed some stuff

--- js/contrastCheck.js
+++ js/contrastCheck.js
@@ -49,7 +49,7 @@ function checkContrast(color) {
 const toggleSwitch = document.querySelector('input[type=checkbox]');
 const contrastWhite = document.querySelector('div#contrastwhite');
 const contrastBlack = document.querySelector('div#contrastblack');
-const contrastValueBlack = document.querySelector('div#contrastblack>span');
+const contrastValueBlack = document.querySelector('span#contrastrange');
 
 toggleSwitch.addEventListener('change', function () {
     if (this.checked) {
