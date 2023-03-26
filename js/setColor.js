278ce531188f036789ea11858125e6ef391eadfe
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-26 15:17:30 +0200

    Update setColor.js

--- js/setColor.js
+++ js/setColor.js
@@ -1,7 +1,8 @@
 function setColor() {
-  var input = document.getElementById("hex-input").value.toUpperCase();
+  var input = document.getElementById("hex-input").value;
+  var inputUpperCase = input.toUpperCase();
   var url = new URL(window.location.href);
-  url.searchParams.set("color", input);
+  url.searchParams.set("color", inputUpperCase);
   url.searchParams.delete("random");
   window.location.href = url.href;
 }
