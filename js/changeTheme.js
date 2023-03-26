c5e7531cbd05afb56258db9d731ae34fdf680f85
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-26 14:04:42 +0200

    Changed the way logo functions

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -5,17 +5,15 @@ function changeTheme(color) {
   
     formField.style.backgroundColor = "#" + color;
     button.style.backgroundColor = "#" + color;
-    logo.style.backgroundColor = "#" + color;
+    logo.style.fill = "#" + color;
     updateLogoColor(color);
 }
 
 function updateLogoColor(color) {
   var formField = document.getElementById("hex-input");
-  var logo = document.getElementById("logo");
   var header = document.querySelector("header");
 
   formField.style.background = "#" + color;
-  logo.style.background = "#" + color;
 
   var r = parseInt(color.substring(0, 2), 16);
   var g = parseInt(color.substring(2, 4), 16);
@@ -23,13 +21,9 @@ function updateLogoColor(color) {
 
   // test if all color channels are at least 150
   if (r >= 150 && g >= 150 && b >= 150) {
-    // set logo to black version and header background to black
-    logo.src = "img/logo-black.png";
     header.style.backgroundColor = "#000000";
     formField.style.color = "#000000"
   } else {
-    // set logo to white version and header background to white
-    logo.src = "img/logo-white.png";
     header.style.backgroundColor = "#FFFFFF";
     formField.style.color = "#FFFFFF"
   }
