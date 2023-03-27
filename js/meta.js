64f09fbc3d37b5f4de5e4d5daaf967ae08bd35c0
Richard Kužela <73738591+RichardKanshen@users.noreply.github.com>
2023-03-27 18:22:38 +0200

    Fixed some more stuff

--- js/meta.js
+++ js/meta.js
@@ -1,5 +1,5 @@
 function setMetaTags(color) {
 
-    const themeColorMetaTag = document.querySelector('meta[property="theme-color"]');
+    const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
     themeColorMetaTag.setAttribute('content', "#" + color);
 }
