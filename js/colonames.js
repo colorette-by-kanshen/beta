d6ff536ad91355b1fb2abe7d1ec880c0a13f1f0f
Richard Kužela <richard@kanshen.click>
2023-07-09 17:23:00 +0200

    Update colonames.js

--- js/colonames.js
+++ js/colonames.js
@@ -1,14 +1,16 @@
 function getColonamesLink(color) {
     const button = document.getElementById("colonamescheckbutton");
+    const nocolor = document.getElementById("unavailableoncolonames")
+    const yescolor = document.getElementById("availableoncolonames")
 
     fetch('https://colonames.by.kanshen.click/' + color)
   .then(response => {
       if (response.status === 404) {
           button.classList.add("COLONAMESmatchnotfound");
-          button.getElementById("unavailableoncolonames").classList.add("valid");
+          nocolor.classList.add("valid");
       } else {
           button.classList.add("COLONAMESmatchfound");
-          button.getElementById("availableoncolonames").classList.add("valid");
+          yescolor.classList.add("valid");
     }
   })
   .catch(error => {
