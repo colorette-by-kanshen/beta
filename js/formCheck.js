16d3957c592f02be0c3aac24cc52e10908e9df47
Richard Kužela <richard@kanshen.click>
2023-07-09 09:42:14 +0200

    Fixed some bugs from 65669f5
    - Fixed incorrect form alert when entering valid codes (OR => AND)
    - Fixed simplifyHEX() returning "undefined" instead of an empty string

--- js/formCheck.js
+++ js/formCheck.js
@@ -7,7 +7,7 @@ function formCheck() {
   }
 
   // Check if input is exactly 6 or 3 hexadecimal characters
-  if (!/^[0-9a-fA-F]{6}$/.test(hex) || !/^[0-9a-fA-F]{3}$/.test(hex)) {
+  if (!/^[0-9a-fA-F]{6}$/.test(hex) && !/^[0-9a-fA-F]{3}$/.test(hex)) {
     alert("Please enter a valid 6-digit / 3-digit hexadecimal color code");
     return;
   }
--- js/generatePalette.js
+++ js/generatePalette.js
@@ -487,7 +487,7 @@ function simplifyHEX(color) {
     if (color[0] === color[1] && color[2] === color[3] && color[4] === color[5]) {
         return "\n\n#" + color[0] + color[2] + color[4];
     } else {
-        return
+        return ""
     }
 }
 
